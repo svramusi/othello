56f0979fb0fa85e955e41227ab96db972fb2ec3f
Steve Ramusivich <svramusi@gmail.com>
2012-10-16 20:29:47 -0400

    Add initial code

---
+++ src/students/ramusivich/StevesOthelloPlayer.java
@@ -0,0 +1,18 @@
+package students.ramusivich;
+
+import edu.drexel.cs.ai.othello.*;
+import java.util.Date;
+
+public class StevesOthelloPlayer extends OthelloPlayer {
+	
+	public StevesOthelloPlayer(String name)
+	{
+		super(name);
+	}
+
+	public Square getMove(GameState currentState, Date deadline)
+	{
+		return null;
+	}
+
+}
