5549bde7ae1fb978b826b3d2d5d97b72f593bbb8
Steve R <svramusi@gmail.com>
2012-10-30 22:16:37 -0400

    Try to optimize it some more
    Can't get the timing right
    Always seem to run out of time towards the end of the game
    Need to fix the average time spent finding successors
    Don't know why it takes so much longer in the end game...

--- src/students/ramusivich/StevesOthelloPlayer.java
+++ src/students/ramusivich/StevesOthelloPlayer.java
@@ -15,12 +15,18 @@ public class StevesOthelloPlayer extends OthelloPlayer {
 
 	private static final double CORNER_SCORE = 100;
 	private static final double NEXT_TO_CORNER_SCORE = -500;
-	private static final double TIME_TO_RUN_MINIMAX = 500;
+
+	private static double longestMiniMaxRun;
+	private static double averageGetSuccessors;
+	private static double totalGetSuccessors;
+	private static double iterationsGetSuccessors;
 	
 	public StevesOthelloPlayer(String name)
 	{
 		super(name);
 		tree = null;
+		longestMiniMaxRun = -1;
+		averageGetSuccessors = 0;
 	}
 	
 	//http://radagast.se/othello/howto.html
@@ -48,10 +54,10 @@ public class StevesOthelloPlayer extends OthelloPlayer {
 		else
 		{
 			try{
-				BufferedWriter out = new BufferedWriter(new FileWriter("output.txt", true));
+				//BufferedWriter out = new BufferedWriter(new FileWriter("output.txt", true));
 				
-				out.write("\n---------------------------------------------------------------");
-				out.write("\ncount before setting a new head: " + tree.Count());
+				//out.write("\n---------------------------------------------------------------");
+				//out.write("\ncount before setting a new head: " + tree.Count());
 				
 				try{
 					tree.SetNewHead(new MiniMaxNode(currentState, 0));
@@ -59,47 +65,73 @@ public class StevesOthelloPlayer extends OthelloPlayer {
 				catch(Exception e)
 				{
 					tree = new MiniMax(new MiniMaxNode(currentState, currentState.getScore(currentState.getCurrentPlayer())));
-					out.write("\n\nFailed to update head to: " + currentState.toString());
+					//out.write("\n\nFailed to update head to: " + currentState.toString());
 				}
 
-				out.write("\ncount after setting a new head: " + tree.Count());
+				//out.write("\ncount after setting a new head: " + tree.Count());
 				
-				out.close();
+				//out.close();
 			} catch (Exception e)
 			{
 				System.out.println("CAUGHT ERROR: " + e.getMessage());
 			}
 		}
-
-		Square nextMove = null;
 		
-		//NEED TO FIND A BETTER WAY TO DO THIS!
-		while(this.getMillisUntilDeadline() > TIME_TO_RUN_MINIMAX)
+		Square nextMove = findNextBest();
+		log("next best move: " + nextMove);
+		
+		while(isEnoughTimeToGoAgain())
 		{
 			getNextRowOfSuccessors();
-
-			nextMove = ((GameState)tree.AlphaBetaSearch().GetObject()).getPreviousMove();
-			//log("setting current best move: " + nextMove.toString());
-			//log("time left: " + this.getMillisUntilDeadline());
-			this.registerCurrentBestMove(nextMove);
+			nextMove = findNextBest();
 		}
 		
 		/* return the move that we have chosen */
 		log("Steve's agent is moving to " + nextMove + "...");
+		log("time wasted: " + this.getMillisUntilDeadline());
 		return nextMove;
+	}
+
+	private Square findNextBest() {
+		long startTime = new Date().getTime();
+		Square nextMove = ((GameState)tree.AlphaBetaSearch().GetObject()).getPreviousMove();
+		long endTime = new Date().getTime();
+		long difference = endTime - startTime;
+		
+		log("longest mini max: " + longestMiniMaxRun);
+		
+		if(difference > longestMiniMaxRun)
+			longestMiniMaxRun = difference;
+		
+		this.registerCurrentBestMove(nextMove);
+		return nextMove;
+	}
+	
+	private boolean isEnoughTimeToGoAgain()
+	{
+		//If there is less time than the time required to get the successors
+		//and twice the time to perform minimax, then return
+
+		log("average successors: " + averageGetSuccessors);
+		log("longest minimax: " + longestMiniMaxRun);
+		
+		if(this.getMillisUntilDeadline() > averageGetSuccessors + (2 * longestMiniMaxRun))
+			return true;
+		else
+			return false;
 	}
 
 	private void getNextRowOfSuccessors() {
 		List<MiniMaxNode> emptyParents = tree.GetEmptyParents();
-		long longestTime = -1;
 		
 		MiniMaxNode node = null;
 		int initialEmptyParentsSize = emptyParents.size();
-		log("init empty parent size: " + initialEmptyParentsSize);
+		
+		log("finding children for # of empty parents: " + initialEmptyParentsSize);
 		
 		for(int i = 0; i < initialEmptyParentsSize; i++)
 		{
-			if(this.getMillisUntilDeadline() < longestTime + TIME_TO_RUN_MINIMAX)
+			if(this.getMillisUntilDeadline() < averageGetSuccessors)
 				return;
 			
 			long startTime = new Date().getTime();
@@ -118,10 +150,19 @@ public class StevesOthelloPlayer extends OthelloPlayer {
 
 			long endTime = new Date().getTime();
 			
-			long difference = endTime - startTime;
-			
-			if(difference > longestTime)
-				longestTime = difference;
+			//Only count it if its for a non-trivial amount of time
+			//Yes, anything greater than 100ms is a non-trivial amount of time
+			//Think about how many ms you wasted reading this...
+			if(endTime - startTime > 100)
+			{
+				totalGetSuccessors += endTime - startTime;
+				iterationsGetSuccessors++;
+				log("time it took to get successors: " + Long.toString(endTime - startTime));
+				log("total: " + totalGetSuccessors);
+				log("iteratsion: " + iterationsGetSuccessors);
+				log("count: " + tree.Count());
+				averageGetSuccessors = totalGetSuccessors/iterationsGetSuccessors;
+			}
 		}
 	}
 	
